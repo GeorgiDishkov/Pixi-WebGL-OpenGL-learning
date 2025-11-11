58a5f5ec41a4141c536da8931a8b2a62014b2de6
Georgi <60046070+GeorgiDishkov@users.noreply.github.com>
2025-11-11 21:07:38 +0000

    WIP

--- StarExplorer/src/updates/updateHUD.ts
+++ StarExplorer/src/updates/updateHUD.ts
@@ -6,26 +6,62 @@ export function updateHUD() {
   const { abilities } = PlayerHUD;
 
   if (onPress(Input.Key1)) {
-    abilities[0].isActive = !abilities[0].isActive;
+    abilities.forEach((ability) => {
+      if (ability.id === Number(Input.Key1)) {
+        ability.isActive = !ability.isActive;
+      } else {
+        ability.isActive = false;
+      }
+    });
   }
 
   if (onPress(Input.Key2)) {
-    abilities[1].isActive = !abilities[1].isActive;
+    abilities.forEach((ability) => {
+      if (ability.id === Number(Input.Key2)) {
+        ability.isActive = !ability.isActive;
+      } else {
+        ability.isActive = false;
+      }
+    });
   }
 
   if (onPress(Input.Key3)) {
-    abilities[2].isActive = !abilities[2].isActive;
+    abilities.forEach((ability) => {
+      if (ability.id === Number(Input.Key3)) {
+        ability.isActive = !ability.isActive;
+      } else {
+        ability.isActive = false;
+      }
+    });
   }
 
   if (onPress(Input.Key4)) {
-    abilities[3].isActive = !abilities[3].isActive;
+    abilities.forEach((ability) => {
+      if (ability.id === Number(Input.Key4)) {
+        ability.isActive = !ability.isActive;
+      } else {
+        ability.isActive = false;
+      }
+    });
   }
 
   if (onPress(Input.Key5)) {
-    abilities[4].isActive = !abilities[4].isActive;
+    abilities.forEach((ability) => {
+      if (ability.id === Number(Input.Key5)) {
+        ability.isActive = !ability.isActive;
+      } else {
+        ability.isActive = false;
+      }
+    });
   }
 
   if (onPress(Input.Key6)) {
-    abilities[5].isActive = !abilities[5].isActive;
+    abilities.forEach((ability) => {
+      if (ability.id === Number(Input.Key6)) {
+        ability.isActive = !ability.isActive;
+      } else {
+        ability.isActive = false;
+      }
+    });
   }
 }
